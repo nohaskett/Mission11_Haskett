e9024e2262b067319f2666808fc050f2e3b2373e
Noah Haskett <nohaskett@gmail.com>
2024-03-22 13:17:39 -0600

    Pagination Finishing Touches

--- Mission11_Haskett/Controllers/HomeController.cs
+++ Mission11_Haskett/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Mission11_Haskett.Models;
+using Mission11_Haskett.Models.ViewModels;
 using System.Diagnostics;
 
 namespace Mission11_Haskett.Controllers
@@ -12,10 +13,26 @@ namespace Mission11_Haskett.Controllers
         {
             _repo = temp;
         }
-        public IActionResult Index()
+        public IActionResult Index(int pageNum)
         {
-            var bookData = _repo.Books;
-            return View(bookData);
+            int pageSize = 10;
+
+            var blah = new BooksListViewModel
+            {
+                Books = _repo.Books
+                .OrderBy(x => x.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PaginationInfo = new PaginationInfo
+                {
+                    CurrentPage = pageNum,
+                    ItemsPerPage = pageSize,
+                    TotalItems = _repo.Books.Count()
+                }
+            };
+
+        return View(blah);
         }
     }
 }
