ba39d0ef602acb773f49f7fd2511a39c549622ed
Noah Haskett <nohaskett@gmail.com>
2024-03-22 12:16:26 -0600

    one more time

--- Mission11_Haskett/Models/OnlineBookstoreContext.cs
+++ Mission11_Haskett/Models/OnlineBookstoreContext.cs
@@ -7,11 +7,5 @@ namespace Mission11_Haskett.Models
         public OnlineBookstoreContext(DbContextOptions<OnlineBookstoreContext> options) : base(options) { }
 
         public DbSet<Book> Books { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Book>().ToTable("Books"); // Map Book entity to the existing "Books" table
-            base.OnModelCreating(modelBuilder);
-        }
     }
 }
